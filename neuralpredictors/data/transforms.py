771e2def3aa9825825965f09f7c8cb0423b54c23
xup5 <43817079+xup5@users.noreply.github.com>
2022-09-18 14:32:06 -0400

    Update transforms.py

--- neuralpredictors/data/transforms.py
+++ neuralpredictors/data/transforms.py
@@ -1,4 +1,8 @@
-from collections import Iterable, namedtuple
+from collections import namedtuple
+try:
+    from collections.abc import Iterable
+except ImportError:
+    from collections import Iterable
 
 import numpy as np
 import torch
